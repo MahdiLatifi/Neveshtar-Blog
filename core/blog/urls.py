5296cb9bbeab7663d43e303e18712e2941113759
MahdiLatifi <mahdilatifi05@gmail.com>
2026-02-12 17:34:13 +0330

    Just show the index template

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
-
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
