64e1fb3c3c70558d75cc2a66912f0af0d7177c4d
MahdiLatifi <mahdilatifi05@gmail.com>
2026-02-12 23:08:54 +0330

    Added search to index page

--- core/blog/views.py
+++ core/blog/views.py
@@ -7,5 +7,22 @@ from .models import Post
 # Create your views here.
 class IndexView(ListView):
     template_name = 'blog/index.html'
-    queryset = Post.objects.filter(status='published').order_by('-published_at')
     context_object_name = 'posts'
+    paginate_by = 6
+
+    def get_queryset(self):
+        queryset = Post.objects.filter(status='published').order_by('-published_at')
+
+        search_query = self.request.GET.get('search')
+        if search_query:
+            from django.db.models import Q
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(content__icontains=search_query)
+            )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_query'] = self.request.GET.get('search', '')
+        return context
