8aa2a74c696f80c7411d6fc2420b159d68ef59f1
MahdiLatifi <mahdilatifi05@gmail.com>
2026-02-12 16:54:22 +0330

    Updated Post model with handle make short_code and migrate

--- core/blog/models/posts.py
+++ core/blog/models/posts.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.crypto import get_random_string
 from accounts.models import Profile
 
 
@@ -7,8 +8,10 @@ class Post(models.Model):
     content = models.TextField()
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     estimated_reading_time = models.IntegerField()
-    view_count = models.IntegerField()
-    shortened_url_code = models.CharField(max_length=255)
+    view_count = models.IntegerField(default=0)
+    short_code = models.CharField(max_length=20, unique=True, blank=True)
+    published_at = models.DateTimeField()
+    status = models.CharField(max_length=100, choices=(('draft', 'Draft'), ('published', 'Published')), default='draft')
     # comments
     # tags
 
@@ -17,3 +20,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_unique_code(self, length):
+        code = get_random_string(length=length)
+        if not Post.objects.filter(short_code=code).exists():
+            return code
+        return self.get_unique_code(length)
+
+    def save(self, *args, **kwargs):
+        if not self.short_code:
+            self.short_code = self.get_unique_code(15)
+        return super().save(*args, **kwargs)
