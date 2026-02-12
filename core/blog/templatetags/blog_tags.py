7542ebdee2d9258b78fe68f6143b8ee92ca56b74
MahdiLatifi <mahdilatifi05@gmail.com>
2026-02-12 20:47:22 +0330

    Added inclusion tag for categories

--- core/blog/templatetags/blog_tags.py
+++ core/blog/templatetags/blog_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.db.models import Count
+
+from ..models import Category
 
 register = template.Library()
 
@@ -8,3 +11,9 @@ def persian_int(english_int):
     devanagari_nums = ('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹')
     number = str(english_int)
     return ''.join(devanagari_nums[int(digit)] for digit in number)
+
+
+@register.inclusion_tag('blog/categories.html')
+def blog_categories():
+    categories = Category.objects.annotate(posts_count=Count('post'))
+    return {'categories': categories}
