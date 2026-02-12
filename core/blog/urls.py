d33280827e8eed885547e5248fa4cfff4b51c966
MahdiLatifi <mahdilatifi05@gmail.com>
2026-02-12 17:47:07 +0330

    Added base.html and move other templates to blog directory

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -3,5 +3,5 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Post
+
 
 # Create your views here.
+class IndexView(ListView):
+    template_name = 'blog/index.html'
+    queryset = Post.objects.filter(status='published')
+    context_object_name = 'posts'
