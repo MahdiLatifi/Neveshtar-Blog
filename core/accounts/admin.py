cdf7444fef1a19f6f00b65c8671ab668d8914f90
MahdiLatifi <mahdilatifi05@gmail.com>
2026-02-12 16:10:10 +0330

    Added User and Profile model to admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'is_superuser', 'is_staff', 'is_active', 'is_verified')
+    list_filter = ('email', 'is_superuser', 'is_staff', 'is_active', 'is_verified')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    fieldsets = (
+        ('Authentication', {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_superuser', 'is_active', 'is_staff', 'is_verified')}),
+        ('Group Permissions', {'fields': ('groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_superuser', 'is_active', 'is_staff'),
+        }),
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name')
