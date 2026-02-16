9576fd32f2d385c30718dc5dd95e51b8bf146ad7
MahdiLatifi <mahdilatifi05@gmail.com>
2026-02-16 13:23:47 +0330

    Update admin and add actions

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -5,10 +5,31 @@ from .models import Post, Category, Tag, Newsletter, Contact, Comment
 
 
 # Register your models here.
+@admin.action(description="Mark selected as featured")
+def mark_selected_featured(modeladmin, request, queryset):
+    queryset.update(is_featured=True)
+
+
+@admin.action(description="Mark selected as Not featured")
+def mark_selected_not_featured(modeladmin, request, queryset):
+    queryset.update(is_featured=False)
+
+
+@admin.action(description="Mark selected as published")
+def mark_selected_published(modeladmin, request, queryset):
+    queryset.update(status='published')
+
+
+@admin.action(description="Mark selected as Not unpublished")
+def mark_selected_unpublished(modeladmin, request, queryset):
+    queryset.update(status='draft')
+
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'user', 'estimated_reading_time', 'short_code', 'view_count', 'is_featured', 'status')
     summernote_fields = ('content',)
+    actions = [mark_selected_featured, mark_selected_not_featured, mark_selected_published, mark_selected_unpublished]
 
 
 @admin.register(Category)
@@ -31,6 +52,12 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'subject', 'message')
 
 
+@admin.action(description="Mark selected as approved")
+def mark_selected_approved(modeladmin, request, queryset):
+    queryset.update(status='approved')
+
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'post', 'content', 'status')
+    actions = [mark_selected_approved]
